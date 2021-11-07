84ea52f059ba3c8fd913b931628d0dd27fd1e0a8
Patrick Longo <ck.longo@bol.com.br>
2021-11-07 14:33:29 -0300

    github auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import SubscribeButton from '~/components/SubscribeButton';
+import { stripe } from '~/services/stripe';
 
 import styles from './index.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -19,9 +28,9 @@ export default function Home() {
 
           <p>
             Get acces to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="girl coding" />
@@ -29,3 +38,23 @@ export default function Home() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JtCquCDILY822v3ZfPve9Jr');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    }).format(price.unit_amount! / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24h
+  };
+};
